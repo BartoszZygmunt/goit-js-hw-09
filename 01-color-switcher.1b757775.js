7e78ba02048650d405a1a961ac9eac152ddcee53
BartoszZygmunt <122611488+BartoszZygmunt@users.noreply.github.com>
2023-06-23 19:54:30 +0000

    Deploying to gh-pages from @ BartoszZygmunt/goit-js-hw-09@df1897548cccff9b37bf16eb261b77e29fa888a0 🚀

---
+++ 01-color-switcher.1b757775.js
@@ -0,0 +1,2 @@
+let t=null;const e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");d.disabled=!0,e.addEventListener("click",()=>{e.disabled=!0,d.disabled=!1,t=setInterval(()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`},1e3)}),d.addEventListener("click",()=>{clearInterval(t),e.disabled=!1,d.disabled=!0});
+//# sourceMappingURL=01-color-switcher.1b757775.js.map
