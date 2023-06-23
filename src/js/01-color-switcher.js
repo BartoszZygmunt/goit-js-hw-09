6627e733d8f4adfc9a23f4e293fddaefb47d33a5
BartoszZygmunt <it@bartoszzygmunt.pl>
2023-06-23 21:35:35 +0200

    up2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const timerID = null;
 
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+
+const onClickStart = () => {
+  button.disabled = true;
+  timerID = setTimeout(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const onClickStop = () => {
+  clearTimeout(timerID);
+  button.disabled = false;
+};
+
+buttonStart.addEventListener('click', onClickStart);
+buttonStop.addEventListener('click', onClickStop);
